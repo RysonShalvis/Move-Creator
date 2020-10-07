129d1187f6e28ba26e51fb8d6b6d6ec348034ebd
Ryson Shalvis <rysonshalvis@Rysons-MacBook-Pro.local>
2020-10-06 23:02:21 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import AddTrickContainer from './addTrickContainer'
+
+function App() {
+  return (
+    <div className="App">
+      <AddTrickContainer />
+    </div>
+  );
+}
+
+export default App;

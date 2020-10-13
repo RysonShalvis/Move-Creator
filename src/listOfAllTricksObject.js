8274cd0ed9b14083f5fc6f83ab8788a39ce53f34
Ryson Shalvis <rysonshalvis@Rysons-MacBook-Pro.local>
2020-10-12 22:48:49 -0600

    add more flips to object

--- src/listOfAllTricksObject.js
+++ src/listOfAllTricksObject.js
@@ -32,6 +32,16 @@ export const listOfAllTricks = {
       recipe: ['sideflip','180'],
     },
     {
+      name: 'frontflip 180',
+      alias: 'front half',
+      recipe: ['frontflip','180'],
+    },
+    {
+      name: 'backflip 180',
+      alias: 'back half',
+      recipe: ['backflip','180'],
+    },
+    {
       name: '360',
       alias: false,
       recipe: ['180','180']
@@ -61,5 +71,87 @@ export const listOfAllTricks = {
       alias: 'side double full',
       recipe: ['sideflip','720'],
     },
+    {
+      name: 'frontflip 360',
+      alias: 'front full',
+      recipe: ['frontflip','360'],
+    },
+    {
+      name: 'frontflip 720',
+      alias: 'front double full',
+      recipe: ['frontflip','720'],
+    },
+    {
+      name: 'backflip 360',
+      alias: 'back full',
+      recipe: ['backflip','360'],
+    },
+    {
+      name: 'backflip 720',
+      alias: 'back double full',
+      recipe: ['backflip','720'],
+    },
+    {
+      name: 'arabian',
+      alias: false, //not sure if alias
+      recipe: ['180','backflip'],
+    },
+    {
+      name: 'vortex backflip',
+      alias: 'full up backflip', 
+      recipe: ['360','backflip'],
+    },
+    {
+      name: 'double vortex backflip',
+      alias: 'double full up backflip', 
+      recipe: ['720','backflip'],
+    },
+    {
+      name: 'dark arabian',
+      alias: 'spiderman flip',
+      recipe: ['180','frontflip'],
+    },
+    {
+      name: 'vortex frontflip',
+      alias: 'full up frontflip', 
+      recipe: ['360','frontflip'],
+    },
+    {
+      name: 'double vortex frontflip',
+      alias: 'double full up frontflip', 
+      recipe: ['720','frontflip'],
+    },
+    {
+      name: 'vortex sideflip',
+      alias: 'full up sideflip', 
+      recipe: ['360','sideflip'],
+    },
+    {
+      name: 'double vortex sideflip',
+      alias: 'double full up sideflip',
+      recipe: ['720','sideflip'],
+    },
+    {
+      name: 'sideflip in, backflip out',
+      alias: 'side in', 
+      recipe: ['sideflip 180','backflip'],
+    },
+    {
+      name: 'front half in, backflip out',
+      alias: 'front half in',
+      recipe: ['720','sideflip'],
+    },
+    {
+      name: 'sideflip in, backflip out',
+      alias: 'side in',
+      recipe: ['720','sideflip'],
+    },
+    {
+      name: 'double backflip',
+      alias: 'dub back',
+      recipe: ['backflip','backflip'],
+    },
+    
+ 
   ]
 }
